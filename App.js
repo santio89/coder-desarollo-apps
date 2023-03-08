7a88f0dd2f920dbff0b2442e2efc3cd770e65592
santio89 <santiolais1989@gmail.com>
2023-03-07 21:16:51 -0300

    small fix

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import ToDoList from './components/ToDoList.js';
@@ -27,11 +27,11 @@ export default function App() {
 
 
   return (
-    <View style={styles.container} onLayout={onLayoutRootView}>
+    <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
       <Header />
       <ToDoList />
       <Footer />
-    </View>
+    </SafeAreaView>
   );
 }
 
